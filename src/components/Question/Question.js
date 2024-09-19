1363638fb21b71ef25d00b7675a467b96dbfc95d
grv-saini-20 <gouravsaini9797@gmail.com>
2024-09-19 22:23:10 +0530

    fix: button-visibility

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,10 +4,12 @@ import "./Question.css";
 
 function Question({question, options, correctAnswer, handleNext }) {
     const [selectedValue, setSelectedValue] = useState("");
+    const [isSelected, setIsSelected] = useState(false);
     const {addCorrectAnswer} = useContext(QuestionContext);
 
     const handleChange = (e) => {
         setSelectedValue(e.target.value)
+        setIsSelected(true);
     }
 
     const handleSubmit = (e) => {
@@ -23,14 +25,16 @@ function Question({question, options, correctAnswer, handleNext }) {
         <h1 className='question'>{decodeURIComponent(question)}</h1>
         <div className='options'>
         {options.map((option,i)=>(
-        <div key={i} className="option">    
-        <input className='option-input' id={option} name="capital" type="radio" value={option} checked={selectedValue === option} onChange={handleChange} />
+        <div key={i} className="option" >    
+        <input className='option-input' id={option} name="quiz" type="radio" value={option} checked={selectedValue === option} onChange={handleChange} />
         <label className='option-value' htmlFor={option}>{option}</label>
         </div>
         ))}
         </div>
-        <h1 className='status'>{!selectedValue ? "" : selectedValue === correctAnswer ? "Correct!" : "Sorry!"}</h1>
+        <h1 className='status'>{!selectedValue ? "" : selectedValue === correctAnswer ? "Correct!" : "Sorry, Try again!"}</h1>
+        {isSelected &&
         <button className='btn' type='submit'>Next Question</button>
+        }
     </form>
   )
 }
